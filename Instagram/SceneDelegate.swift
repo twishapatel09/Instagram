6935ea271af3f1b9f319270d06824bd2e0d8af92
Twisha Patel <twishapatel09@gmail.com>
2020-03-26 22:55:24 -0700

    changes

--- Instagram/SceneDelegate.swift
+++ Instagram/SceneDelegate.swift
@@ -19,16 +19,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
-        guard let _ = (scene as? UIWindowScene) else { return }
-//        guard let windowScene = (scene as? UIWindowScene) else { return }
-//        if PFUser.current() != nil{
-//            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//            self.window = UIWindow(windowScene: windowScene)
-//            self.window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "FeedViewController")
-//            print("window %@", window)
-//            self.window?.makeKeyAndVisible()
-//        }
-       
+       // guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        if PFUser.current() != nil{
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            self.window = UIWindow(windowScene: windowScene)
+            self.window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "FeedViewController")
+            print("window %@", window)
+            self.window?.makeKeyAndVisible()
+        }
+
 
     }
     
